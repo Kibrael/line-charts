4eb606822fad08ca626fda101291d8691e7ae70e
Andrew <awolfe76@gmail.com>
2015-12-29 13:19:00 -0500

    set default value

--- src/js/index.js
+++ src/js/index.js
@@ -7,38 +7,7 @@ get('data/bank/type/state/data.json', testing);
 function testing(response) {
   if (check(response.statusCode)) {
     var json = JSON.parse(response.text);
-    var config = {
-      bindto: '#chart',
-      data: {
-        type: json.type,
-        json: json.data.years,
-        colors: {
-          count: '#2CB34A'
-        },
-        keys: {
-          x: 'year',
-          value: ['count']
-        }
-      },
-      axis: {
-        x: {
-          label: {
-            position: 'outer-center',
-            text: 'Year'
-          }
-        },
-        y: {
-          label: {
-            position: 'outer-middle',
-            text: 'Count'
-          }
-        }
-      },
-      legend: {
-          show: false
-      }
-    };
-    line(config);
+    line(json);
   } else {
     console.log(response.statusCode)
   }
--- src/js/modules/renderLineChart.js
+++ src/js/modules/renderLineChart.js
@@ -1,7 +1,39 @@
 var c3 = require('c3');
 
-function renderChart(config) {
-    c3.generate(config);
+function renderChart(json, value) {
+  value = typeof value !== 'undefined' ? value : 'count';
+
+  c3.generate({
+    bindto: '#chart',
+    data: {
+      type: json.type,
+      json: json.data.years,
+      colors: {
+        count: '#2CB34A'
+      },
+      keys: {
+        x: 'year',
+        value: [value]
+      }
+    },
+    axis: {
+      x: {
+        label: {
+          position: 'outer-center',
+          text: 'Year'
+        }
+      },
+      y: {
+        label: {
+          position: 'outer-middle',
+          text: value
+        }
+      }
+    },
+    legend: {
+      show: false
+    }
+  });
 }
 
 module.exports = renderChart;
