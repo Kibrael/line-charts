b33891412db66de6a03bbfe8ba1f2670fc0c4cee
Andrew <awolfe76@gmail.com>
2016-01-15 15:52:20 -0500

    smooth transitions

--- src/js/renderLineChart.js
+++ src/js/renderLineChart.js
@@ -1,5 +1,5 @@
 var c3 = require('c3');
-
+var chart;
 function setYLabel(key) {
   var label = 'Count';
   if (key === 'loan_amount') {
@@ -18,61 +18,88 @@ function setColors(value) {
 
 function renderChart(json, value) {
   value = typeof value !== 'undefined' ? value : 'count';
-
-  c3.generate({
-    bindto: '#chart',
-    padding: {
-      top: 10,
-      right: 15,
-    },
-    data: {
-      type: 'area',
+  if (chart) {
+    console.log('test');
+    console.log(json.data.years);
+    console.log(value);
+    chart.load({
       json: json.data.years,
-      colors: setColors(value),
       keys: {
         x: 'year',
         value: [value]
-      }
-    },
-    grid: {
-      /*x: {
-        show: true
-      },*/
-      y: {
-        show: true
-      }
-    },
-    axis: {
-      x: {
-        padding: {
-          left: .1,
-          right: .1
-        },
-        label: {
-          position: 'outer-center',
-          text: 'Year'
+      },
+      colors: setColors(value),
+      /*data: {
+        type: 'area',
+        json: json.data.years,
+        colors: setColors(value),
+        keys: {
+          x: 'year',
+          value: [value]
+        }
+      }*/
+      unload: true
+    });
+  } else {
+    console.log(json.data.years);
+    chart = c3.generate({
+      bindto: '#chart',
+      transition: {
+        duration: 500
+      },
+      padding: {
+        top: 10,
+        right: 15,
+      },
+      data: {
+        type: 'area',
+        json: json.data.years,
+        colors: setColors(value),
+        keys: {
+          x: 'year',
+          value: [value]
         }
       },
-      y: {
-        padding: {
-          top: 10,
-          bottom: 0
+      grid: {
+        /*x: {
+          show: true
+        },*/
+        y: {
+          show: true
+        }
+      },
+      axis: {
+        x: {
+          padding: {
+            left: .1,
+            right: .1
+          },
+          label: {
+            position: 'outer-center',
+            text: 'Year'
+          }
         },
-        min: 0,
-        label: {
-          position: 'outer-middle',
-          text: setYLabel(value)
-        },
-        tick: {
-          format: function (d) { return d.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+        y: {
+          padding: {
+            top: 10,
+            bottom: 0
+          },
+          min: 0,
+          label: {
+            position: 'outer-middle',
+            text: setYLabel(value)
+          },
+          tick: {
+            format: function (d) { return d.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+            }
           }
         }
+      },
+      legend: {
+        show: false
       }
-    },
-    legend: {
-      show: false
-    }
-  });
+    });
+  }
 }
 
 module.exports = renderChart;
